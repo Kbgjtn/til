aac6baf5b5cadaf053c00c1c78c8358b7258dc5f
Kbgjtn <Kambingjan31@gmail.com>
2024-01-10 22:35:29 +0000

    fix: delay theme navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -25,7 +25,7 @@ export function NavBar() {
     };
   }, []);
   return (
-    <nav className="flex-no-wrap dark:bg-black/60 fixed z-50 top-0 bg-transparent flex flex-col w-full items-center justify-between py-2 dark:shadow-lg backdrop-blur-md shadow-none dark:shadow-black/10 lg:flex-wrap lg:justify-start lg:py-4 transition-all delay-1000">
+    <nav className="flex-no-wrap dark:bg-black/60 fixed z-50 top-0 bg-transparent flex flex-col w-full items-center justify-between py-2 dark:shadow-lg backdrop-blur-md shadow-none dark:shadow-black/10 lg:flex-wrap lg:justify-start lg:py-4">
       <div className="flex flex-col items-center">
         <Logo className="max-md:w-9" size={44} />
         <Link className="max-md:text-xs" href="/">
